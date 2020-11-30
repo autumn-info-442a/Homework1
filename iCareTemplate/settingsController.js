794ceb77e1f301022357f6ffab07a7ebb4dd53c3
Jimmy <jimhua32@uw.edu>
2020-11-30 13:30:33 -0800

    latest updates

--- iCareTemplate/settingsController.js
+++ iCareTemplate/settingsController.js
@@ -13,23 +13,58 @@
         // TODO functionality to parse and display settings
         // This function interacts with SettingsView to display the page with appropriate data
     }
+
+    /**
+     * Custom messages are stored as an array of objects with keys "content" and "status"
+     * The key for custom messages within the storage is "custom"
+     * 
+     * Premade messages are stored as an object with keys as the categories
+     * Each category's value is an array of objects with keys "content" and "status"
+     * The key for premade messages within the storage is "premade"
+     */
     
     // pre: accepts message to be added as input
     // post: message is added
-    function addMessage(message) {
-    // TODO functionality to read and add custom message incl error handling incl error handling
+    function addMessage() {
+        let message = $("new_message").value;
+        if (message.length > 50) {
+            alert("Messages cannot be more than 50 characters!");
+            return;
+        }
+        let messages = chrome.storage.local.get("custom");
+        messages.push({content: message, status: true});
+        chrome.storage.local.set({"custom" : messages}, refreshTable()); // TODO Add helper function to update premade table
     }
     
     // pre: accepts message to be removed as input
     // post: message is removed
-    function removeMessage(message) {
-        // TODO functionality to remove a message incl error handling
+    function removeMessage(id) {
+        let messages = chrome.storage.local.get("custom");
+        let message = $(id).value;
+        for (let i = 0; i < messages.length; i++) {
+            if (messages[i].content == message) {
+                messages.splice(i, 1);
+                break;
+            }
+        }
+        chrome.storage.local.set({"custom" : messages}, refreshTable());
     }
     
+    // Should we just remove this function? Seems like extra work on the HTML/JS side for little gain
+
     // pre: accepts a message to be edited (message1) and the new text (message2) as input
     // post: message is updated/edited
-    function editMessage(message1, message2) {
-        // TODO functionality to edit a message incl error handling
+    function editMessage(id) {
+        let messages = chrome.storage.local.get("custom");
+        let message = $(id).value;
+        let newMessage = $("edit_message").value; // should grab this from an edit message box
+        for (let i = 0; i < messages.length; i++) {
+            if (messages[i].content == message) {
+                messages[i] = newMessage;
+                break;
+            }
+        }
+        chrome.storage.local.set({"custom" : messages}, refreshTable());
     }
     
     // pre: a new watch threshold (1-5) to be added as input
@@ -40,14 +75,52 @@
     
     // pre: a currently existing premade message
     // post: message status is toggled
-    function updatePremadeMessage(message) {
-        // TODO functionality to toggle status of a message incl error handling
+    function updatePremadeMessage(category, id) {
+        // premade messages are saved in this scheme
+        // array of booleans with index corresponding to id of message
+        // a second array with the actual message with index corresponding to id
+        let message = $(id).value;
+        let toggle = $(id).checked;
+        let messages = chrome.storage.local.get("premade");
+        messages = messages[category];
+        for (let i = 0; i < messages.length; i++) {
+            if (messages[i].content == message) {
+                messages[i].status = toggle;
+                break;
+            }
+        }
+        chrome.storage.local.set({"premade" : messages});
     }
+
+    // Category status is stored as an array of objects with keys "category" and "status"
     
     // pre: accepts a category to be toggled
     // post: category status is changed
-    function updateCategory(category) {
-        // TODO functionality to toggle status of a category incl error handling
+    function updateCategory(category, id) {
+        let toggle = $(id).checked;
+        let categories = chrome.storage.local.get("category");
+        for (let i = 0; i < categories.length; i++) {
+            if (categories[i].category == category) {
+                categories[i].status = toggle;
+                break;
+            }
+        }
+        chrome.storage.local.set({"category" : categories});
+    }
+
+    function refreshTable() {
+        // TODO functionality to refresh the custom table contents
+        let table = $("custom_table");
+        let messages = chrome.storage.local.get("custom");
+        let newBody = document.createElement("tbody");
+        for (var m in messages) {
+            let row = newBody.insertRow();
+            let cell1 = row.insertCell();
+            let cell2 = row.insertCell(); // edit
+            let cell3 = row.insertCell(); // remove
+            cell1.innerText = m;
+        }
+        table.replaceChild(newBody, table.querySelector("tbody"));
     }
 
     /**
@@ -64,20 +137,42 @@
 // post: return a Random message 
 function getRandomMessage() {
     //TODO: Randomly select a message from all selected pools.
-    return ["A random message"]
+    let premade = chrome.storage.local.get("premade");
+    let custom = chrome.storage.local.get("custom");
+    let categories = chrome.storage.local.get("category");
+    let allMessages = [];
+    for (let i = 0; i < categories.length; i++) {
+        if (categories[i].status == true) {
+            let tempMessages = premade[categories[i].category];
+            for (let j = 0; j < tempMessages.length; j++) {
+                if (tempMessages[j].status == true) {
+                    allMessages.push(tempMessages[j].content);
+                }
+            }
+        }
+    }
+    for (let i = 0; i < custom.length; i++) {
+        if (custom[i].status == true) {
+            allMessages.push(custom[i].content);
+        }
+    }
+    return allMessages[Math.floor(Math.random() * allMessages.length)];
 }
   
-  // pre: no input 
-  // post: return all of the current setting as an JSON object
+// pre: no input 
+// post: return all of the current setting as an object
 function getCurrentSettingsAndData() {
-    //TODO: Return both current settings about selected message pool, watched time threshold, and all the messages (both pre-made and custom message) 
-    return ["alliCareSetting"]
+    return {
+        threshold: chrome.storage.local.get("threshold"),
+        premadeMessages: chrome.storage.local.get("premade"),
+        customMessages: chrome.storage.local.get("custom"),
+        categories: chrome.storage.local.get("category")
+    };
 }
   
-  // pre: no input 
-  // post: returns the current watch threshold
+// pre: no input 
+// post: returns the current watch threshold
 function getThreshold() {
-    // TODO: Update the newThreshold value to the MonitorController
-    return threshold;
+    return chrome.storage.local.get("threshold");
 }
   
