3988d766443252992aaf6aa2fd02ea8fcc3bba60
Jimmy <jimhua32@uw.edu>
2020-12-08 14:37:47 -0800

    added oninstall and fixed additional bugs

--- iCareTemplate/components/monitor/background.js
+++ iCareTemplate/components/monitor/background.js
@@ -2,6 +2,12 @@ console.log("background.js activated")
 console.log(window.location.href)
 
 
+chrome.runtime.onInstalled.addListener(function(details) {
+  if (details.reason == "install") {
+      setInitialSettings();
+  }
+});
+
 window.addEventListener('load', () => {
   let videoLinks = document.getElementsByClassName("yt-simple-endpoint")
   console.log(videoLinks)
@@ -10,7 +16,50 @@ window.addEventListener('load', () => {
 
   // post: threshold is updated
 function newThreshold() {
-    watchThreshold = 0; 
+  watchThreshold = 0; 
+}
+
+function setInitialSettings() {
+  chrome.storage.local.set({"category": [
+    {
+      category: "motivational",
+      status: true
+    },
+    {
+      category: "screentime",
+      status: true
+    },
+    {
+      category: "exercise",
+      status: true
+    },
+    {
+      category: "custom",
+      status: true
+    }
+  ]});
+  chrome.storage.local.set({"premade" : {
+    motivational: [
+      {
+        content: "Believe in your dreams!",
+        status: true
+      }
+    ],
+    screentime: [
+      {
+        content: "Have you been outside today?",
+        status: true
+      }
+    ],
+    exercise: [
+      {
+        content: "Have you exercised today?",
+        status: true
+      }
+    ]
+  }});
+  chrome.storage.local.set({"custom": []});
+  chrome.storage.local.set({"threshold" : 3});
 }
   
 // matched url pattern: https://www.youtubekids.com/watch?v=sLuSq2RL9X0
--- iCareTemplate/components/setting/settingsController.js
+++ iCareTemplate/components/setting/settingsController.js
@@ -1,19 +1,31 @@
 (function() {
     "use strict";
 
+    
     window.addEventListener("load", initialize);
 
     function initialize() {
+        chrome.storage.local.get(function(result) {
+            console.log(result);
+        });
+        displaySettings();
         $("add").addEventListener("click", addMessage);
-        chrome.storage.local.clear(); // FOR TESTING PURPOSES ONLY
-        // Add more event listeners
-        chrome.storage.local.set({"custom": []}); // purely for testing, remove later
+        $("threshold").addEventListener("change", updateWatchThreshold);
+        let checkboxes = document.querySelectorAll("#categories input");
+        for (let i = 0; i < checkboxes.length; i++) {
+            checkboxes[i].addEventListener("change", function() {
+                updateCategory(checkboxes[i].id);
+            });
+        }
     }
 
     // pre: no inputs
     // post: all settings are displayed
     function displaySettings() {
+        displayThreshold();
+        displayCategory();
         refreshTable();
+        displayPremade();
     }
 
     /**
@@ -33,8 +45,12 @@
             alert("Messages cannot be more than 50 characters!");
             return;
         }
+        if (message.length == 0) {
+            alert("Messages cannot be empty!");
+            return;
+        }
         chrome.storage.local.get("custom", function(result) {
-            if (result.custom.length >= 15) {
+            if (result.custom.length > 15) {
                 alert("Cannot add more than 15 messages!");
                 return;
             }
@@ -42,14 +58,14 @@
             chrome.storage.local.set({"custom" : result.custom});
             $("new_message").value = "";
             refreshTable();
-        }); // TODO Add helper function to update premade table
+        });
     }
     
     // pre: accepts message to be removed as input
     // post: message is removed
     function removeMessage(id) {
         console.log("removing message");
-        chrome.storage.local.get("custom", function(result) {
+        chrome.storage.local.get("custom", function(result) { // shouldn't ever be called without a message existing
             let message = $(id).value;
             console.log("message is " + message);
             let messages = result.custom;
@@ -71,7 +87,7 @@
     // pre: accepts a message to be edited (message1) and the new text (message2) as input
     // post: message is updated/edited
     function editMessage(id) {
-        chrome.storage.local.get("custom", function(result) {
+        chrome.storage.local.get("custom", function(result) { // shouldn't ever be called without a message existing
             let message = $(id).value;
             let messages = result.custom;
             let newMessage = $("edit_message").value;
@@ -93,26 +109,35 @@
     // pre: a new watch threshold (1-5) to be added as input
     // post: watch threshold is updated
     function updateWatchThreshold() {
-        chrome.storage.local.set({"Threshold": $("threshold").value});
+        chrome.storage.local.set({"threshold": $("threshold").value});
     }
     
     // pre: a currently existing premade message
     // post: message status is toggled
-    function updatePremadeMessage(category, id) {
-        // premade messages are saved in this scheme
-        // array of booleans with index corresponding to id of message
-        // a second array with the actual message with index corresponding to id
-        let message = $(id).value;
-        let toggle = $(id).checked;
+    function updatePremadeMessage(category, message_id, box_id) {
+        let message = $(message_id).value;
+        let toggle = box_id.checked;
         chrome.storage.local.get("premade", function(result) {
+            console.log(result);
             let messages = result.premade[category];
             for (let i = 0; i < messages.length; i++) {
                 if (messages[i].content == message) {
                     messages[i].status = toggle;
-                    break;
+                    checkIfMessageRemains().then(function(result) {
+                        if (!result && !toggle) {
+                            alert("Cannot have less than 1 message enabled");
+                            box_id.checked = true;
+                            messages[i].status = !toggle;
+                            return;
+                        } else {
+                            chrome.storage.local.set({"premade" : result.premade}, function() {
+                                console.log("done changing message " + message + " to " + toggle);
+                            });
+                        }
+                    });
                 }
             }
-            chrome.storage.local.set({"premade" : messages});
+            console.log(result);
         });
     }
 
@@ -120,34 +145,105 @@
     
     // pre: accepts a category to be toggled
     // post: category status is changed
-    function updateCategory(category, id) {
+    function updateCategory(id) {
         let toggle = $(id).checked;
         chrome.storage.local.get("category", function(result) {
             let categories = result.category;
+            console.log(categories);
+            let enabled = 0;
             for (let i = 0; i < categories.length; i++) {
-                if (categories[i].category == category) {
+                if (categories[i].status && categories[i].category != "custom") {
+                    enabled++;
+                }
+            }
+            if (enabled <= 1 && !toggle) {
+                $(id).checked = true;
+                alert("Cannot have less than one category other than custom enabled!");
+                return;
+            }
+            for (let i = 0; i < categories.length; i++) {
+                if (categories[i].category == $(id).value) {
                     categories[i].status = toggle;
                     break;
                 }
             }
+            console.log(categories);
             chrome.storage.local.set({"category" : categories});
         });
     }
 
+    function displayThreshold() {
+        chrome.storage.local.get("threshold", function(result) {
+            $("threshold").value = result.threshold;
+        });
+    }
+
+    function displayCategory() {
+        chrome.storage.local.get("category", function(result) {
+            let categories = result.category;
+            for (let i = 0; i < categories.length; i++) {
+                let category = categories[i];
+                $(category.category).checked = category.status;
+            }
+        });
+    }
+
+    function displayPremade() {
+        let table = $("premade_table");
+        chrome.storage.local.get("premade", function(result) {
+            let newBody = document.createElement("tbody");
+            let messages = result.premade;
+            let categories = Object.keys(messages);
+            for (let i = 0; i < categories.length; i++) {
+                let cat = newBody.insertRow();
+                let cell = cat.insertCell();
+                cat.value = categories[i];
+                cat.classList.add("category_header");
+                switch (categories[i]) {
+                    case "motivational":
+                        cell.innerHTML = "Motivational";
+                        break;
+                    case "exercise":
+                        cell.innerHTML = "Exercise";
+                        break;
+                    case "screentime":
+                        cell.innerHTML = "Screen Time";
+                        break;
+                    default:
+                        break;
+                }
+                for (let j = 0; j < messages[categories[i]].length; j++) {
+                    let m = messages[categories[i]][j].content;
+                    let row = newBody.insertRow();
+                    row.id = categories[i] + " " + j;
+                    row.value = m;
+                    let cell1 = row.insertCell(); // message
+                    let cell2 = row.insertCell(); // "show" checkbox
+                    cell1.innerHTML = m;
+                    let show = document.createElement("input");
+                    show.setAttribute("type", "checkbox");
+                    show.checked = messages[categories[i]][j].status;
+                    show.addEventListener("click", function() {
+                        updatePremadeMessage(categories[i], row.id, this);
+                    });
+                    cell2.appendChild(show);
+                }
+            }
+            table.replaceChild(newBody, table.querySelector("tbody"));
+        });
+    }
+
     function refreshTable() {
-        // TODO functionality to refresh the custom table contents
         let table = $("custom_table");
-        console.log("refreshing the table!");
-        chrome.storage.local.get("custom", function(result) {
+        chrome.storage.local.get({custom: []}, function(result) {
             let newBody = document.createElement("tbody");
             let messages = result.custom;
-            console.log(messages);
             for (let i = 0; i < messages.length; i++) {
                 let m = messages[i].content;
                 let row = newBody.insertRow();
                 row.id = "custom " + i;
                 row.value = m;
-                let cell1 = row.insertCell();
+                let cell1 = row.insertCell(); // message
                 let cell2 = row.insertCell(); // edit
                 let cell3 = row.insertCell(); // remove
                 cell1.innerText = m;
@@ -169,9 +265,37 @@
                     removeMessage(row.id);
                 });
                 cell3.appendChild(remove);
-                console.log("adding " + m);
             }
             table.replaceChild(newBody, table.querySelector("tbody"));
+        });
+    }
+
+    function checkIfMessageRemains() {
+        return new Promise(resolve => {
+            chrome.storage.local.get(["premade", "custom", "category"], function(result) {
+                let premade = result.premade;
+                let custom = result.custom;
+                let categories = result.category;
+                let allMessages = [];
+                for (let i = 0; i < categories.length - 1; i++) {
+                    if (categories[i].status == true) {
+                        let tempMessages = premade[categories[i].category];
+                        for (let j = 0; j < tempMessages.length; j++) {
+                            if (tempMessages[j].status == true) {
+                                allMessages.push(tempMessages[j].content);
+                            }
+                        }
+                    }
+                }
+                if (categories[categories.length - 1].status == true) { // custom is the last in the array
+                    for (let i = 0; i < custom.length; i++) {
+                        if (custom[i].status == true) {
+                            allMessages.push(custom[i].content);
+                        }
+                    }
+                }
+                resolve(allMessages.length > 1);
+            });
         });
     }
 
@@ -187,13 +311,13 @@
 
 // pre: no input 
 // post: return a Random message 
-export function getRandomMessage() {
+function getRandomMessage() {
     chrome.storage.local.get(["premade", "custom", "category"], function(result) {
         let premade = result.premade;
         let custom = result.custom;
         let categories = result.category;
         let allMessages = [];
-        for (let i = 0; i < categories.length; i++) {
+        for (let i = 0; i < categories.length - 1; i++) {
             if (categories[i].status == true) {
                 let tempMessages = premade[categories[i].category];
                 for (let j = 0; j < tempMessages.length; j++) {
@@ -203,9 +327,11 @@ export function getRandomMessage() {
                 }
             }
         }
-        for (let i = 0; i < custom.length; i++) {
-            if (custom[i].status == true) {
-                allMessages.push(custom[i].content);
+        if (categories[categories.length - 1].status == true) { // custom is the last in the array
+            for (let i = 0; i < custom.length; i++) {
+                if (custom[i].status == true) {
+                    allMessages.push(custom[i].content);
+                }
             }
         }
         return allMessages[Math.floor(Math.random() * allMessages.length)];
